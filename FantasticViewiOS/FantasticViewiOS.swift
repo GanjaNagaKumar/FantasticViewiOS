cfd50ed0b75862780a1df3f16d905c388187a5cc
Kumar <ganja.nagakumar@gmail.com>
2024-05-12 20:28:16 +0530

    init

---
+++ FantasticViewiOS/FantasticViewiOS.swift
@@ -0,0 +1,18 @@
+//
+//  FantasticViewiOS.swift
+//  FantasticViewiOS
+//
+//  Created by NagaKumar Ganja on 12/05/24.
+//
+
+import SwiftUI
+
+struct FantasticViewiOS: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FantasticViewiOS()
+}
---
+++ FantasticViewiOS/FantasticViewiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FantasticViewiOSApp.swift
+//  FantasticViewiOS
+//
+//  Created by NagaKumar Ganja on 12/05/24.
+//
+
+import SwiftUI
+
+@main
+struct FantasticViewiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
